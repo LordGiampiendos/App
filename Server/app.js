1ce3955e15902bddd192000239349e1823873939
LordGiampiendos <85945128+LordGiampiendos@users.noreply.github.com>
2021-08-15 21:57:59 +0200

    Update app.js

--- Server/app.js
+++ Server/app.js
@@ -22,7 +22,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(function (req, res, next) {
 
     // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8888');
+    res.setHeader('Access-Control-Allow-Origin', 'https://autoscuolab.vercel.app/');
 
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
